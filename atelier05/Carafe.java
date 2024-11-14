6fb6a013abb835adc67a0f07d692c10ecb56fa12
lemar <lemar@PC-kLisa>
2024-11-14 09:58:12 +0100

    carafe

---
+++ atelier05/Carafe.java
@@ -0,0 +1,19 @@
+package atelier05;
+
+public class Carafe {
+	
+	public Carafe() {
+		
+	}
+	
+	public void Remplir() {}
+	
+	public void Vider() {}
+	
+	public void Contenu() {}
+	
+	public void Capacité() {}
+	
+	public void Transvaser() {}
+		
+}
