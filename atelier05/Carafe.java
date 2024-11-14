eda1d9838ed640ccc588fda7bc073b0686dfaefd
lemar <lemar@PC-kLisa>
2024-11-14 10:19:39 +0100

    elle a ressuscité

---
+++ atelier05/Carafe.java
@@ -0,0 +1,23 @@
+package atelier05;
+
+public class Carafe {
+	private int  contenu_, capacite_;
+	
+	/** Création d'une carafe vide
+	 * @param capacite capacité en litres de la carafe */
+	public Carafe(int capacite) {
+		this.capacite_ = capacite;
+		this.contenu_ = 0;
+	}
+	
+	public void Remplir() {}
+	
+	public void Vider() {}
+	
+	public void Contenu() {}
+	
+	public void Capacité() {}
+	
+	public void Transvaser() {}
+		
+}
---
+++ atelier05/testCarafe.java
@@ -0,0 +1,9 @@
+package atelier05;
+
+public class testCarafe {
+	/** point d'entrée d'un exécutable */
+	public static void main(String[] args) { 
+		Carafe c = new Carafe(5);
+	}
+
+}
