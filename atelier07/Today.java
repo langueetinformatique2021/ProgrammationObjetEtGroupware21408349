8546e0cacb9a7785d9b4e587b4fb8727b2a30bc6
lemar <lemar@PC-kLisa>
2024-12-05 11:12:52 +0100

    squelette exo 4

--- atelier07/Today.java
+++ atelier07/Today.java
@@ -3,7 +3,6 @@ package atelier07;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
-import java.util.SimpleTimeZone;
 import java.util.TimeZone;
 
 public class Today {
@@ -21,9 +20,9 @@ public class Today {
 		 calendar.setTime(trialTime);
 		
 		 // Afficher la date courante
-		 System.out.println("DAY_OF_MONTH: " + calendar.get(Calendar.DAY_OF_MONTH));
-		 System.out.println("MONTH: " + calendar.get(Calendar.MONTH));
 		 System.out.println("YEAR: " + calendar.get(Calendar.YEAR));
+		 System.out.println("MONTH: " + calendar.get(Calendar.MONTH));
+		 System.out.println("DAY_OF_MONTH: " + calendar.get(Calendar.DAY_OF_MONTH));
 		 System.out.println("HOUR_OF_DAY: " + calendar.get(Calendar.HOUR_OF_DAY));
 		 System.out.println("MINUTE: " + calendar.get(Calendar.MINUTE));
 		 System.out.println("SECOND: " + calendar.get(Calendar.SECOND));
