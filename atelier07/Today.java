bf3068da50da072e4c149258b6a5ee237cfd3dc2
lemar <lemar@PC-kLisa>
2024-12-05 10:49:12 +0100

    exo 3

--- atelier07/Today.java
+++ atelier07/Today.java
@@ -21,9 +21,9 @@ public class Today {
 		 calendar.setTime(trialTime);
 		
 		 // Afficher la date courante
-		 System.out.println("YEAR: " + calendar.get(Calendar.YEAR));
-		 System.out.println("MONTH: " + calendar.get(Calendar.MONTH));
 		 System.out.println("DAY_OF_MONTH: " + calendar.get(Calendar.DAY_OF_MONTH));
+		 System.out.println("MONTH: " + calendar.get(Calendar.MONTH));
+		 System.out.println("YEAR: " + calendar.get(Calendar.YEAR));
 		 System.out.println("HOUR_OF_DAY: " + calendar.get(Calendar.HOUR_OF_DAY));
 		 System.out.println("MINUTE: " + calendar.get(Calendar.MINUTE));
 		 System.out.println("SECOND: " + calendar.get(Calendar.SECOND));
