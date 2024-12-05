59c8999c698260ac78b058657f260bc2fc4f4e82
lemar <lemar@PC-kLisa>
2024-12-05 10:03:07 +0100

    semaine 11

--- atelier06/Felin.java
+++ atelier06/Felin.java
@@ -1,29 +1,33 @@
 package atelier06;
 
 /** Création et gestion de félins */
-
 public class Felin extends Animal {
 // ajout d'attributs propres à la sous-classe
 	protected boolean domestique = false;
 
-/** le constructeur de Félins fait appel au constructeur de la sur-classe Animal */
+	/**
+	 * le constructeur de Félins fait appel au constructeur de la sur-classe Animal
+	 */
 	public Felin(String type) { // les félins ont 4 pattes
 		super(type, 4);
-}
+	}
 
-/** présentation des caractéristiques du félin */
+	/** présentation des caractéristiques du félin */
 	public void présente() {
 // appel de la méthode de la sur-classe
 		super.présente();
 		String etat = (domestique) ? "domestique" : "sauvage";
 		System.out.println("je suis vraiment un animal " + etat);
-}
+	}
 
-/** cri du félin */
+	/** cri du félin */
 	public void crie() {
-		System.out.println("Je suis un félin");
-}
+		String cri = (domestique) ? "miaule" : "rugis";
+		System.out.println("Je suis un félin et je " + cri);
 
-	public String nom();{
+	}
+
+	public String nom() {
 		return null;
+	}
 }
---
+++ atelier06/testFelin.java
@@ -0,0 +1,13 @@
+package atelier06;
+
+public class testFelin {
+	
+	/** point d'entrée d'un exécutable */
+	public static void main(String[] args) { 
+	Felin f = new Felin("lions");
+	f.présente();
+	f.crie();
+	}
+	
+
+}
